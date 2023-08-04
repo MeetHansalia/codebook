d1d7a331f9b2c8c9c62f4c412ac9bb6ba3afa411
meet_hansalia <meethansalia99@gmail.com>
2023-08-04 15:04:37 +0530

    initial setup

---
+++ codebook/src/App.js
@@ -0,0 +1,19 @@
+
+import { Fragment } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Fragment>
+      <Header/>
+      <div>
+        <h1>Meet</h1>
+      </div>
+      <Footer/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ codebook/src/components/Header.js
@@ -0,0 +1,14 @@
+import logo from '../assets/logo.png'
+
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt="" className="logo"></img>
+    </div>
+  )
+}
+
+export default Header
